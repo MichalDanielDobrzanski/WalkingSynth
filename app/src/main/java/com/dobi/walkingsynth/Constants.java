8ddbdfd7163987b96e7828cf1ed929f918a29f3d
Michał Dobrzański <scoobiedoobielbn@gmail.com>
2015-10-19 16:41:10 +0200

    Oscilloscope is close enough.

--- app/src/main/java/com/dobi/walkingsynth/Constants.java
+++ app/src/main/java/com/dobi/walkingsynth/Constants.java
@@ -7,7 +7,9 @@ public final class Constants {
 
     public static final int ACC_MAGNITUDE = 0;
     public static final int ACC_GRAV_DIFF = 1;
-    public static final int STEP_DETECTOR = 2;
+    public static final int ACC_ALL = 2;
+
+    public static final int SERIES_COUNT = 3;
 
     private Constants() {
         //this prevents even the native class from

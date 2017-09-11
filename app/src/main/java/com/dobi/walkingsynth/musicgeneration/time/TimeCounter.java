b0e2b30f6c04714948f50c0b43a9ca60c07c3d7c
Michał Dobrzański <dobrzanski.michal.daniel@gmail.com>
2017-09-11 17:21:04 +0200

    fixed time counter, improved UI

--- app/src/main/java/com/dobi/walkingsynth/musicgeneration/time/TimeCounter.java
+++ app/src/main/java/com/dobi/walkingsynth/musicgeneration/time/TimeCounter.java
@@ -3,6 +3,7 @@ package com.dobi.walkingsynth.musicgeneration.time;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -14,6 +15,8 @@ import java.util.TimerTask;
  * Timer for showing how long the music is being played.
  */
 public class TimeCounter {
+
+    public static final String TAG = TimeCounter.class.getSimpleName();
 
     private static final int ONE_SECOND = 1000;
 
@@ -29,19 +32,21 @@ public class TimeCounter {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                final long newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000;
+                final long newValue = (SystemClock.elapsedRealtime() - mInitialTime);
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                       textViewWeakReference.get().setText(convertToString(newValue));
+                        textViewWeakReference.get().setText(convertMillisecondsToHumanReadable(newValue));
+                        Log.d(TAG, "TimeCounter: view update. New value: " + newValue);
                     }
                 });
             }
         }, ONE_SECOND, ONE_SECOND);
+        Log.d(TAG, "TimeCounter: created.");
     }
 
 
-    private String convertToString(long milliseconds) {
+    private String convertMillisecondsToHumanReadable(long milliseconds) {
         int seconds = (int) (milliseconds / 1000);
         int minutes = seconds / 60;
         seconds = seconds % 60;
